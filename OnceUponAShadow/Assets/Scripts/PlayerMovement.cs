fec3e734687ae803c552f86f2cde584be396923e
Timo <timo@overmark.org>
2018-01-08 17:31:14 +0200

    Play sounds also on remote host

--- OnceUponAShadow/Assets/Scripts/PlayerMovement.cs
+++ OnceUponAShadow/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 using UnityEngine.Networking;
 
+
 public class PlayerMovement : NetworkBehaviour
 {
     [SyncVar]
@@ -13,10 +14,12 @@ public class PlayerMovement : NetworkBehaviour
     private Rigidbody2D m_Rigidbody2D;
 	private float jumping;
 	private AudioSource audioSource;
+	NetworkView nView;
 
     void Start () {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
 		audioSource = GetComponent<AudioSource> ();
+		nView = GetComponent<NetworkView> ();
     }
 
 	void Update () {
@@ -29,7 +32,8 @@ public class PlayerMovement : NetworkBehaviour
 
 		if (Input.touchCount > 0 || Input.GetButtonDown("Fire1")) {
 			Jump ();
-			audioSource.Play();
+			CmdPlaySound();
+			playSound();
 		}
     }
 
@@ -65,4 +69,23 @@ public class PlayerMovement : NetworkBehaviour
         theScale.x *= -1;
         transform.localScale = theScale;
     }
+
+	/*public void playSound()
+	{
+		nView.RPC("rpcPlaySound", RPCMode.Others);
+	}*/
+
+	void playSound()	{
+		audioSource.volume = 1f;
+		audioSource.Play();
+
+	}
+
+	// Play that soundsource over the network.
+	[Command]
+	void CmdPlaySound()	{
+		audioSource.volume = 0.5f;
+		audioSource.Play();
+
+	}
 }
