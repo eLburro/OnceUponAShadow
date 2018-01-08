1ca70f9ef966d06e864841e7d767b250f9d4d349
eL burro <ramon.burri@gmail.com>
2018-01-08 16:26:31 +0100

    animations started

--- OnceUponAShadow/Assets/Scripts/PlayerMovement.cs
+++ OnceUponAShadow/Assets/Scripts/PlayerMovement.cs
@@ -11,12 +11,11 @@ public class PlayerMovement : NetworkBehaviour
 
     private float m_MaxSpeed = 10f;
     private Rigidbody2D m_Rigidbody2D;
-	private float jumping;
-	private AudioSource audioSource;
+    private Animator m_Anim;
 
     void Start () {
+        m_Anim = GetComponent<Animator>();
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
-		audioSource = GetComponent<AudioSource> ();
     }
 
 	void Update () {
@@ -24,38 +23,33 @@ public class PlayerMovement : NetworkBehaviour
         {
             return;
         }
-			
-		Move();
 
-		if (Input.touchCount > 0 || Input.GetButtonDown("Fire1")) {
-			Jump ();
-			audioSource.Play();
-		}
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        Move(h);
     }
 
-    private void Move()
-    {
+    private void Move(float move)
+    {       
+        /*if (move != 0)
+        {
+            m_Anim.SetBool("moving", true);
+        }
+        else
+        {
+            m_Anim.SetBool("moving", false);
+        }*/
 
-		float xVel = CrossPlatformInputManager.GetAxis("Horizontal");
-		if(Input.accelerationEventCount > 0 && Mathf.Abs(Input.acceleration.x) > 0) {
-			xVel = 6 * Input.acceleration.x;
-			m_Rigidbody2D.velocity = new Vector2(xVel * m_MaxSpeed, m_Rigidbody2D.velocity.y);
-		}
-		else m_Rigidbody2D.velocity = new Vector2(xVel * m_MaxSpeed, m_Rigidbody2D.velocity.y);
+        m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
 
-        if (xVel > 0 && !m_FacingRight)
+        if (move > 0 && !m_FacingRight)
         {           
             Flip();
         }
-        else if (xVel < 0 && m_FacingRight)
+        else if (move < 0 && m_FacingRight)
         {
             Flip();
         }
     }
-
-	void Jump() {
-		m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x,6);
-	}
 
     void Flip()
     {
