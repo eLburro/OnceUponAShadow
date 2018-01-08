d44e1ce72458b32e5fa8f9f01a621cbad29c4333
Timo <timo@overmark.org>
2018-01-08 11:47:49 +0200

    Timo

---
+++ OnceUponAShadow/Assets/AccelerometerInputBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerometerInputBehaviour : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	private AudioSource audioSource;
+	public AudioClip scream;
+	private float distance;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		audioSource = GetComponent<AudioSource> ();
+		Screen.autorotateToPortrait = true;
+		Screen.orientation = ScreenOrientation.AutoRotation;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//transform.Translate(0, 0, (Input.acceleration.z*-0.1f)+0.9f);
+		distance = (((Input.acceleration.z-0.5f)*0.1f)+0.1f)+0.2f;
+		transform.localScale = new Vector3(scale,scale, 0);
+		transform.Translate(0, scale, 0);
+		rb.velocity = new Vector2(6*Input.acceleration.x, -3*(Input.acceleration.z+0.5f));
+
+		/*for (int i = 0; i < Input.touchCount; ++i) {
+			if (Input.GetTouch (i).phase == TouchPhase.Began) {*/
+		if(Input.touchCount > 0) {
+			audioSource.Play();
+		//	}
+		}
+	}
+}
---
+++ OnceUponAShadow/Assets/DebugText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugText : MonoBehaviour {
+
+	public Text textF;
+
+	// Use this for initialization
+	void Start () {
+		textF = GetComponent <Text> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float scale = (((Input.acceleration.z-0.5f)*0.1f)+0.1f)+0.2f;
+		textF.text = "inputX = "+Input.acceleration.x;
+		textF.text += "\ninputY = "+Input.acceleration.y;
+		textF.text += "\ninputZ = "+Input.acceleration.z;
+		textF.text += "\ntouchCount = "+Input.touchCount;
+		textF.text += "\nscale = "+scale;
+	}
+
+}
