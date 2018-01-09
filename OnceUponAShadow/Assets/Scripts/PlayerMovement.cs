7a5dcec11aac5b8598b6a3f88b608234d0984cc8
Timo <timo@overmark.org>
2018-01-09 13:20:31 +0200

    Anims out of movement script

--- OnceUponAShadow/Assets/Scripts/PlayerMovement.cs
+++ OnceUponAShadow/Assets/Scripts/PlayerMovement.cs
@@ -51,9 +51,9 @@ public class PlayerMovement : NetworkBehaviour
         }
 
 		if (xVel > 0.2 || xVel < -0.2) {
-			m_Anim.SetBool("moving", true);
+			//m_Anim.SetBool("moving", true);
 		} else {
-			m_Anim.SetBool("moving", false);
+			//m_Anim.SetBool("moving", false);
 		}
     }
 
@@ -68,11 +68,6 @@ public class PlayerMovement : NetworkBehaviour
 
 	[Command]
 	void CmdFlip()	{
-		/*m_FacingRight = !m_FacingRight;
-
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;*/
 		Flip ();
 	}
 
