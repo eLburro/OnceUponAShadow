5857d43e090a0987379b3ea4ead128cd2f2afb1b
eL burro <ramon.burri@gmail.com>
2018-01-10 17:51:05 +0100

    added player canvas to players and moved scripts

---
+++ OnceUponAShadow/Assets/Scripts/AddPlayerCanvas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class AddPlayerCanvas : NetworkBehaviour
+{
+    public GameObject playerCanvas;
+
+    void Start()
+    {
+        if (!isServer)
+        {
+            GameObject go = Instantiate(playerCanvas, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+        }
+    }
+}
