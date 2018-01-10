f64bc694a2fdb2da468a258c56001c61113aaf52
Timo <timo@overmark.org>
2018-01-10 19:42:59 +0100

    Scream state for 2 seconds

--- OnceUponAShadow/Assets/Scripts/PrincessActions.cs
+++ OnceUponAShadow/Assets/Scripts/PrincessActions.cs
@@ -8,31 +8,13 @@ public class PrincessActions : NetworkBehaviour	{
 	private AudioSource audioSource;
 	private Animator m_Anim;
 
-	//shake gesture
-	float accelerometerUpdateInterval = 1.0f / 60.0f;
-	float lowPassKernelWidthInSeconds = 2.0f;
-	float shakeDetectionThreshold = 4.0f;
-	float lowPassFilterFactor;
-	Vector3 lowPassValue;
 
 	void Start()
 	{
 		audioSource = GetComponent<AudioSource>();
 		m_Anim = GetComponent<Animator>();
 
-		//shake gesture
-		lowPassFilterFactor = accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
-		shakeDetectionThreshold *= shakeDetectionThreshold;
-		lowPassValue = Input.acceleration;	
-
-		Vector3 acceleration = Input.acceleration;
-		lowPassValue = Vector3.Lerp(lowPassValue, acceleration, lowPassFilterFactor);
-		Vector3 deltaAcceleration = acceleration - lowPassValue;
-
-		if (deltaAcceleration.sqrMagnitude >= shakeDetectionThreshold)
-		{
-			//putOutFire ();
-		}	
+		m_Anim.SetBool ("isScreaming", false);
 	}
 
 	void Update()
@@ -41,20 +23,23 @@ public class PrincessActions : NetworkBehaviour	{
 		{
 			return;
 		}
-
+			
 		if (Input.touchCount > 0 || Input.GetKeyDown(KeyCode.Space))
 		{
-			m_Anim.SetBool ("isScreaming", true);
-			CmdPlaySound();
-			playSound();
-			//yield return new WaitForSeconds(2);
-			m_Anim.SetBool ("isScreaming", false);
+			StartCoroutine (Scream());
 		}
 	}
-		
+	IEnumerator Scream() {
+		m_Anim.SetBool ("isScreaming", true);
+		CmdPlaySound ();
+		playSound ();
+		yield return new WaitForSeconds(2);
+		m_Anim.SetBool ("isScreaming", false);
+	}
+
 	void playSound()
 	{
-		audioSource.volume = 1f;
+		audioSource.volume = 0.75f;
 		audioSource.Play();
 	}
 
@@ -62,7 +47,7 @@ public class PrincessActions : NetworkBehaviour	{
 	[Command]
 	void CmdPlaySound()
 	{
-		audioSource.volume = 0.5f;
+		audioSource.volume = 0.35f;
 		audioSource.Play();
 
 	}
